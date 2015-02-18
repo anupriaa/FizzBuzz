481d2085984a034aea6344bc91fa7ec67e0045a0
Anupriya Srivastava <anupriya@hawaii.edu>
2015-02-17 21:55:24 -1000

    fixed

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,17 +1,32 @@
 /**
- * Created by Anupriya on 2/4/2015.
+ * Solves the FizzBuzz Problem.
  */
 public class FizzBuzz {
+  /**
+   * This program plays the game "Fizzbuzz".
+   * It counts to 100, replacing each multiple of 5 with the word "Buzz",
+   * each multiple of 3 with the word "Fizz",
+   * and each multiple of both with the word "FizzBuzz".
+   * @param args Ignored.
+   */
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
-      if ((i % 3 == 0) && (i % 5 == 0))
+      if ((i % 3 == 0) && (i % 5 == 0)) {
         System.out.println(i + " FizzBuzz");
-      else if(i % 3 == 0) {
-        System.out.println(i + " Fizz");
-      }else if (i % 5 == 0)
-        System.out.println(i + " Buzz");
-      else
-        System.out.println(i);
+      }
+      else {
+        if (i % 3 == 0) {
+          System.out.println(i + " Fizz");
+        }
+        else {
+          if (i % 5 == 0) {
+            System.out.println(i + " Buzz");
+          }
+          else {
+            System.out.println(i);
+          }
+        }
+      }
     }
   }
 }
